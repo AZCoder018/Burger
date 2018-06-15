ea3ba91db07abcf83fea2d4e19e671521177d148
AZCoder018 <jason.meadows.esq@hotmail.com>
2018-06-14 21:39:01 -0700

    added sql db and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name) VALUES ('Baconator Burger');
+INSERT INTO burgers (burger_name) VALUES ('Tripple Tripple Burger');
+INSERT INTO burgers (burger_name) VALUES ('Buffalo Burger');
+INSERT INTO burgers (burger_name) VALUES ('Whopper Senior Burger');
+INSERT INTO burgers (burger_name) VALUES ('Half Pounder Burger');
+INSERT INTO burgers (burger_name) VALUES ('Heart Attack Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Mama Burger');
+INSERT INTO burgers (burger_name) VALUES ('Beyond Meat Beast Burger');

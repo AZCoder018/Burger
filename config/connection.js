1afec07a92346750e6cbd54d3bed5166fc733630
AZCoder018 <jason.meadows.esq@hotmail.com>
2018-06-14 22:35:02 -0700

    added connection.js

---
+++ config/connection.js
@@ -0,0 +1,34 @@
+//Node Connection to MySQL
+
+var mysql = require ("mysql");
+
+var connection;
+
+//JAWSDB Connection for Heroku; if not, MySQL database
+if (process.env.JAWSDB_URL)
+{
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else
+{
+	connection = mysql.createConnection(
+	{
+		host: "localhost",
+		user: "root",
+		password: "DrPepper",
+		database: "burgers_db"
+	});
+};
+
+
+connection.connect(function(err) 
+{
+  if (err) 
+  {
+    console.error("error connecting: " + err.stack);
+    return;
+  };
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
